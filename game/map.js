e4cb35dbbdf11f69bc710173107e204263a27dfc
Hydrothermal <hydrothermal@tuta.io>
2019-06-26 21:07:45 -0400

    Add directional bias to map generator

--- game/map.js
+++ game/map.js
@@ -1,14 +1,15 @@
-const { randRange } = require("../util.js");
+const { randRange, directions, direction_diffs } = require("../util.js");
 
 class Cell {
-    constructor(map, x, y) {
+    constructor(map, x, y, direction) {
         this.map = map;
         this.x = x;
         this.y = y;
+        this.direction = direction;
     }
 
     get neighbors() {
-        let neighbors = [[-1, 0], [0, 1], [1, 0], [0, -1]].map(([x, y]) => this.getRelative(x, y)).filter(cell => !cell.oob);
+        let neighbors = directions.map(dir => this.getRelative(dir)).filter(cell => !cell.oob);
 
         return neighbors;
     }
@@ -17,8 +18,9 @@ class Cell {
         return this.x < 0 || this.y < 0 || this.x >= this.map.width || this.y >= this.map.height;
     }
 
-    getRelative(x_diff, y_diff) {
-        return new Cell(this.map, this.x + x_diff, this.y + y_diff);
+    getRelative(direction) {
+        let [x_diff, y_diff] = direction_diffs[direction];
+        return new Cell(this.map, this.x + x_diff, this.y + y_diff, direction);
     }
 
     is(x, y) {
@@ -32,31 +34,32 @@ class Cell {
 
 function carve(cell) {
     let map = cell.map;
+    let next = map.head.getRelative(map.head.direction);
     let free = cell.neighbors.filter(n => !map.get(n.x, n.y));
 
     if(free.length !== 0) {
-        map.head = free.random();
-        map.board.push(map.head);
+        if(Math.random() < 0.65 && !next.oob && !map.get(next.x, next.y)) {
+            map.head = next;
+        } else {
+            map.head = free.random();
+        }
 
+        map.board.push(map.head);
         carve(map.head);
     }
+
+    map.visualize();
 }
 
 class Map {
     constructor(width, height) {
-        let start_side = ["v", "h"].random();
         this.width = width;
         this.height = height;
-
-        if(start_side === "v") {
-            this.head = new Cell(this, randRange(0, width - 1), [0, height - 1].random());
-        } else {
-            this.head = new Cell(this, [0, width - 1].random(), randRange(0, height - 1));
-        }
-
+        this.head = new Cell(this, Math.floor(width / 2), Math.floor(height / 2));
         this.start = this.head;
         this.board = [this.head];
 
+        this.head.direction = directions.random();
         carve(this.head);
     }
 
@@ -66,20 +69,23 @@ class Map {
 
     visualize() {
         let neighbors = this.start.neighbors;
+        let output = "";
 
         for(let y = 0; y < this.height; y++) {
             for(let x = 0; x < this.width; x++) {
                 if(this.start.is(x, y)) {
-                    process.stdout.write("[+]");
+                    output += ("[+]");
                 } else if(this.get(x, y)) {
-                    process.stdout.write("[#]");
+                    output += ("[#]");
                 } else {
-                    process.stdout.write("[ ]");
+                    output += ("[ ]");
                 }
             }
 
-            process.stdout.write("\n");
+            output += ("\n");
         }
+
+        console.log(output);
     }
 }
 
--- util.js
+++ util.js
@@ -1,3 +1,11 @@
+const directions = ["north", "east", "south", "west"];
+const direction_diffs = {
+    "north": [0, -1],
+    "east":  [+1, 0],
+    "south": [0, +1],
+    "west":  [-1, 0]
+};
+
 Array.prototype.random = function() {
     return this[Math.floor(Math.random() * this.length)];
 };
@@ -6,4 +14,4 @@ function randRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-module.exports = { randRange };
+module.exports = { randRange, directions, direction_diffs };
